097d7338f9290eb7a3b3ec2d4afbb1f2bc100a87
Niral Pokal <niralpokal@gmail.com>
2016-04-25 12:37:23 -0700

    I added a signup route and cookie route so that the user can login.

--- app.js
+++ app.js
@@ -9,27 +9,83 @@ var url = " mongodb://lift:lift@ds019471.mlab.com:19471/lift"
 app.use(express.static('./public'));
 app.use(cookieParser());
 
+function User(user){
+  this.firstName = user.firstName;
+  this.lastName = user.lastName;
+  this.username = user.username;
+  this.pass = user.pass;
+  this.workoutPlan = 0;
+  this.goal = "";
+  this.weight = user.weight;
+  this.height = user.height;
+  this.age = user.age;
+  this.pic = user.pic;
+}
+
+
 app.post('/login', jsonParser, function(req, res) {
   var user = req.body;
   MongoClient.connect(url, function(err, db){
     if (err){
-      throw err;
+      res.sendStatus(503);
+      db.close();
     }else{
       db.collection('users').find(user).toArray(function(err, docs){
         if(err){
-          throw err
+          res.sendStatus(404)
+          db.close();
         }else{
           res.cookie('remember', true, {expires: new Date(Date.now()+ 900000)})
           res.cookie('id', docs[0]._id);
           res.cookie('user', docs[0].username);
-          res.send(docs[0]);
+          res.sendStatus(200);
+          db.close();
         }
       })
     }
   })
-
 });
 
-app.post('/singup', jsonParser, function(req, res) {
+app.post('/user', jsonParser, function(req, res) {
+  var user = new User(req.body);
+  MongoClient.connect(url, function(err, db){
+    if (err){
+      res.sendStatus(503);
+      db.close();
+    }else{
+      db.collection('users').insert([user], function(err, docs){
+        if(err){
+          res.sendStatus(404)
+          db.close();
+        }else{
+          res.cookie('remember', true, {expires: new Date(Date.now()+ 900000)})
+          res.cookie('id', docs[0]._id);
+          res.cookie('user', docs[0].username);
+          res.sendStatus(200);
+          db.close();
+        }
+      })
+    }
+  })
+});
 
+app.get('/user', function(req, res) {
+  var user = {
+    _id:ObjectID(req.cookies.id);
+  };
+  MongoClient.connect(url, function(err,db){
+    if(err){
+      res.sendStatus(503);
+      db.close();
+    }else{
+      db.collection('users').find(user).toArray(function(err, docs){
+        if (err){
+          throw err;
+        }else{
+          res.send(docs[0]);
+          db.close();
+        }
+      })
+    }
+  })
 });
