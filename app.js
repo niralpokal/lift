f18f344a90d9fb5f63faa316aca70352d9129def
Niral Pokal <niralpokal@gmail.com>
2016-04-25 13:04:03 -0700

    I added a delete user route.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function User(user){
   this.weight = user.weight;
   this.height = user.height;
   this.age = user.age;
-  this.pic = user.pic;
+  //this.pic = user.pic;
 }
 
 
@@ -60,14 +60,14 @@ app.post('/user', jsonParser, function(req, res) {
       res.sendStatus(503);
       db.close();
     }else{
-      db.collection('users').insert([user], function(err, docs){
+      db.collection('users').insert([user], function(err, results){
         if(err){
           res.sendStatus(404)
           db.close();
         }else{
           res.cookie('remember', true, {expires: new Date(Date.now()+ 900000)})
-          res.cookie('id', docs[0]._id);
-          res.cookie('user', docs[0].username);
+          res.cookie('id', results.ops[0]._id;
+          res.cookie('user', results.ops[0].username);
           res.sendStatus(200);
           db.close();
         }
@@ -96,3 +96,33 @@ app.get('/user', function(req, res) {
     }
   })
 });
+
+app.delete('/user', function(req,res){
+  var user = {
+    _id:ObjectID(req.cookies.id)
+  }
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      throw err
+    }else{
+      db.collection('users').remove(user, function(err, results){
+        if (err){
+          throw err;
+        }else{
+          res.clearCookie('id');
+          res.clearCookie('user');
+          res.clearCookie('remember')
+          res.sendStatus(200);
+          db.close();
+        }
+      })
+    }
+  })
+});
+
+
+if(!require.main.loaded){
+  var server = app.listen(8080)
+}
+
+module.exports = app;
