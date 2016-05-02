f35ef04a26fab2f696e9f23f1734dd0f3ba351e9
Niral Pokal <niralpokal@gmail.com>
2016-05-02 16:20:54 -0700

    I updated the testing for mocha. #5

--- app.js
+++ app.js
@@ -88,11 +88,11 @@ app.post('/user', jsonParser, function(req, res) {
           res.sendStatus(404)
           db.close();
         }else{
-          db.close();
           res.cookie('remember', true, {expires: new Date(Date.now()+ 900000)})
           res.cookie('id', results.ops[0]._id);
           res.cookie('user', results.ops[0].username);
           res.sendStatus(200);
+          db.close();
         }
       })
     }
@@ -113,6 +113,9 @@ app.get('/user', function(req, res) {
           db.close();
         }else{
           db.close();
+          res.cookie('remember', true, {expires: new Date(Date.now()+ 900000)})
+          res.cookie('id', docs[0]._id);
+          res.cookie('user', docs[0].username);
           res.send(docs[0]);
         }
       })
@@ -134,8 +137,8 @@ app.put('/user', jsonParser, function(req,res){
           res.sendStatus(404);
           db.close();
         }else{
-          db.close();
           res.sendStatus(200);
+          db.close();
         }
       })
     }
@@ -155,11 +158,11 @@ app.delete('/user', function(req,res){
           res.sendStatus(404);
           db.close();
         }else{
-          db.close();
           res.clearCookie('id');
           res.clearCookie('user');
           res.clearCookie('remember')
           res.sendStatus(200);
+          db.close();
         }
       })
     }
@@ -189,9 +192,7 @@ app.post('/plan', jsonParser, function(req, res) {
 
 app.get('/plan', function(req, res) {
   var plan = {
-    users:{
-      id:req.cookies.id
-    }
+    creator:ObjectID(req.cookies.id)
   }
   MongoClient.connect(url, function(err, db){
     if(err){
@@ -202,8 +203,8 @@ app.get('/plan', function(req, res) {
           res.sendStatus(404);
           db.close();
         }else{
-          db.close();
           res.send(docs[0]);
+          db.close();
         }
       })
     }
