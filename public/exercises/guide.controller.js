2fbd20c1a2daca8af5f4ec9111a8274a6c37d4a3
Niral Pokal <niralpokal@gmail.com>
2016-05-06 13:59:33 -0700

    I populated a modal with a step by step guide of doing the exercise a person uses.#6

--- public/exercises/guide.controller.js
+++ public/exercises/guide.controller.js
@@ -3,8 +3,13 @@ app.controller('guideController', guide);
 app.$inject = ['$scope', '$uibModalInstance', 'Exercise'];
 
 function guide($scope, $uibModalInstance, Exercise){
-  $scope.guide = exercise
+  var vm = this
+  vm.info = Exercise
+  console.log($scope.guide);
   $scope.go= function(path){
     console.log();
+  }
+  vm.close = function(){
+    $uibModalInstance.close()
   }
 }
--- server/tests/public/exercises/guide.controller.js
+++ server/tests/public/exercises/guide.controller.js
@@ -3,7 +3,9 @@ app.controller('guideController', guide);
 app.$inject = ['$scope', '$uibModalInstance', 'Exercise'];
 
 function guide($scope, $uibModalInstance, Exercise){
-  $scope.guide = exercise
+  var vm = this
+  vm.info = Exercise
+  console.log($scope.guide);
   $scope.go= function(path){
     console.log();
   }
