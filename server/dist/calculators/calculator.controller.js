0cfcefc007e4ec74a9a8f79658d37fd5c2db5c27
Niral Pokal <niralpokal@gmail.com>
2016-06-07 17:23:38 -0700

    I updated the BMI calculator to include the catagory of bmi. #28

--- server/dist/calculators/calculator.controller.js
+++ server/dist/calculators/calculator.controller.js
@@ -8,6 +8,16 @@ function calculator($scope){
     var height = data.height;
     var weight = data.weight;
     var solution = (weight/(height*height)) * 703;
-    vm.bmi = solution;
+    var round = solution.toFixed(2);
+    if(round < 18.50){
+      vm.cat = "Underweight"
+    } else if(round < 24.90){
+      vm.cat = "Normal Weight"
+    } else if (round < 29.90){
+      vm.cat = "Overweight"
+    }else{
+      vm.cat = "Obesity"
+    }
+    vm.bmi = round
   }
 }
